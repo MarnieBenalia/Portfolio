d3d4fe97c087595ccf1dff1627fbb971ded33876
Marnie Benalia <marnie.benalia@gmail.com>
2023-05-04 09:49:53 +0200

    workdayone

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,5 @@
 const BUTTONHAM = document.querySelector('.hm-button');
-/*const FORM = document.querySelector('.menu');*/
+//const FORM = document.querySelector('.menu-nav');
 
 BUTTONHAM.addEventListener('click',()=>{
     const ISOPENED = BUTTONHAM.getAttribute('aria-expended');
@@ -7,10 +7,10 @@ BUTTONHAM.addEventListener('click',()=>{
 
     if (ISOPENED === 'false') {
         BUTTONHAM.setAttribute('aria-expended', 'true');
-        //FORM.style.marginLeft = "0%";
+       // FORM.style.marginLeft = "0%";
     }
     else{
         BUTTONHAM.setAttribute('aria-expended', 'false');
-        //FORM.style.marginLeft = "-200%";
+       // FORM.style.marginLeft = "-200%";
     }  
 })
